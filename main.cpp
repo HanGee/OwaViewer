8355ac84d244fd839d8887db780011acb2564228
Fred Chien <cfsghost@gmail.com>
2014-10-26 06:36:39 +0800

    support to use "App.qml" to be default QML file

--- main.cpp
+++ main.cpp
@@ -2,25 +2,37 @@
 #include <QQmlApplicationEngine>
 #include <QDir>
 #include <QFile>
+#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
+	QApplication app(argc, argv);
+
+	QQmlApplicationEngine engine;
+
 	if (argc < 2) {
 		printf("Usage: OwaViewer [directory]\n");
 		return 1;
 	}
 
 	QDir dirPath(argv[1]);
-	QString qmlPath = dirPath.filePath("app.qml");
+	QString qmlPath;
 
-	if (!QFile(qmlPath).exists()) {
-		printf("OwaNEXT Application doesn't exist.\n");
-		return 1;
+	// It's a directory
+	if (QFileInfo(dirPath.path()).isDir()) {
+
+		// Trying to read QML file in the directory
+		qmlPath = dirPath.filePath("app.qml");
+		if (!QFile(qmlPath).exists()) {
+
+			qmlPath = dirPath.filePath("App.qml");
+			if (!QFile(qmlPath).exists()) {
+				printf("OwaNEXT Application doesn't exist.\n");
+				return 1;
+			}
+		}
 	}
 
-	QApplication app(argc, argv);
-
-	QQmlApplicationEngine engine;
 	engine.load(QUrl(qmlPath));
 
 	return app.exec();
